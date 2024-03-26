ba769d777b595050b15c59ca6adcce336af76c6a
Shawn Goldwasser <shawn.goldwasser@gmail.com>
2024-03-26 08:20:51 -0400

    Rename host to daemon

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -5,7 +5,7 @@ async function main() {
   await prisma.setting.create({ data: { name: 'admin_user', value: 'admin' } })
   await prisma.setting.create({ data: { name: 'admin_pass', value: hashPassword('admin') } })
 
-  await prisma.host.create({ data: { name: 'Local Docker Host', path: '/var/run/docker.sock' } })
+  await prisma.daemon.create({ data: { name: 'Local Docker Host', path: '/var/run/docker.sock' } })
 
   await prisma.$disconnect()
 }
