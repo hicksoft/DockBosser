9ff8c0e35792e52f2a1e12107ea41aab69476042
Shawn Goldwasser <shawn.goldwasser@gmail.com>
2024-03-29 15:13:46 -0400

    Rename daemon to host

---
+++ backend/src/docker/host.test.ts
@@ -0,0 +1,27 @@
+/*
+ * @group integration/docker
+ */
+
+import { generateRandomString } from '../../test/utils'
+import { getHost, isOnline, createNetwork, removeNetwork, getNetworks } from './host'
+
+describe('Docker host', () => {
+  it('Is online', async () => {
+    const host = getHost()
+    await expect(isOnline(host)).resolves.toBe(true)
+  })
+
+  it('Remove network', async () => {
+    const name = generateRandomString(32)
+    console.log(name)
+
+    const host = getHost()
+    const network = await createNetwork(host, { name, id: 1 })
+    const preNetworks = await getNetworks(host)
+    expect(preNetworks.filter((n) => n.Id === network.id)).toHaveLength(1)
+
+    await removeNetwork(host, 1)
+    const postNetworks = await getNetworks(host)
+    expect(postNetworks.filter((n) => n.Id === network.id)).toHaveLength(0)
+  })
+})
---
+++ backend/src/docker/host.ts
@@ -0,0 +1,47 @@
+import Dockerode from 'dockerode'
+
+interface ICreateNetworkConfig {
+  name: string
+  id: number
+}
+
+export function getHost() {
+  return new Dockerode()
+}
+
+export async function isOnline(host: Dockerode) {
+  try {
+    await host.version()
+  } catch (e) {
+    return false
+  }
+  return true
+}
+
+export async function getContainers(host: Dockerode) {
+  return host.listContainers({ all: true })
+}
+
+export async function getNetworks(host: Dockerode) {
+  return host.listNetworks()
+}
+
+export async function getImages(host: Dockerode) {
+  return host.listImages()
+}
+
+export async function getVolumes(host: Dockerode) {
+  return host.listVolumes()
+}
+
+export async function createNetwork(host: Dockerode, config: ICreateNetworkConfig) {
+  return host.createNetwork({
+    Name: config.name,
+    Driver: 'bridge',
+    Labels: { boss_id: config.id.toString() },
+  })
+}
+
+export async function removeNetwork(host: Dockerode, networkId: number) {
+  return host.pruneNetworks({ filters: { label: [`boss_id=${networkId}`] } })
+}
